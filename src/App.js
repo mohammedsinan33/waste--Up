06bcf62599d8f5da4f55fb2a2b0f2ce6a278a3e0
Mohammed Sinan K <mohammedsinan.k.33@gmail.com>
2023-10-07 23:15:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="nav_bar">
+          <div className="right">
+            <h1>Waste up</h1>
+          </div>
+          <div className="left">
+            <p>solutions</p>
+            <p>contact</p>
+            <p>About</p>
+            <button>Sign in</button>
+          </div>
+        </div>
       </header>
+      <main>
+        <div className="centre">
+          <h1>Waste Management</h1>
+          <br />
+          <h1>Platform</h1>
+        </div>
+        <div className='first boox'>
+              
+        </div>
+      </main>
+      <footer>
+        <div className="svg">
+          <img src="Untitled.png" alt="" />
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
